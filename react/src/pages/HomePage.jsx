763d8f885cd834fcec5e603a8752fd5a20d06659
Developerzakir <zakir1234@!za>
2025-11-03 07:16:29 +0600

    Home page categories,collections product,new arrival section design in react project

--- react/src/pages/HomePage.jsx
+++ react/src/pages/HomePage.jsx
@@ -1,11 +1,17 @@
 import React, { Component, Fragment } from 'react'
 import FeaturedProduct from '../components/home/FeaturedProduct'
+import { Categories } from './../components/home/Categories';
+import { Collections } from './../components/home/Collections';
+import { NewArrival } from './../components/home/NewArrival';
 
 export class HomePage extends Component {
   render() {
     return (
       <Fragment>
-        <FeaturedProduct />
+         <FeaturedProduct />
+         <NewArrival />
+         <Collections />
+         <Categories />
       </Fragment>
     )
   }
