085074dc65c5e946040b75efa1aa5366a05f2025
Developerzakir <zakir1234@!za>
2025-11-03 07:58:08 +0600

    Home page mega menu, nav menu & slider design in react project

--- react/src/components/home/MegaMenu.jsx
+++ react/src/components/home/MegaMenu.jsx
@@ -1,11 +1,82 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
-export class MegaMenu extends Component {
+class MegaMenu extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeIndex: null,
+    };
+  }
+
+  handleToggle = (index) => {
+    this.setState((prevState) => ({
+      activeIndex: prevState.activeIndex === index ? null : index,
+    }));
+  };
+
   render() {
+    const { activeIndex } = this.state;
+
+    // Example category data
+    const menuItems = [
+      "Men's Clothing",
+      "Women's Clothing",
+      "Electronics",
+      "Shoes",
+      "Accessories",
+      "Home & Kitchen",
+      "Accessories",
+      "Home & Kitchen",
+      "Sports",
+      "Beauty",
+      "Sports",
+      "Beauty",
+    ];
+
     return (
-      <div>MegaMenu</div>
-    )
+      <div className="accordionMenuDiv">
+        <div className="accordionMenuDivInside">
+          {menuItems.map((title, index) => (
+            <div key={index}>
+              <button
+                className={`accordion ${activeIndex === index ? "active" : ""}`}
+                onClick={() => this.handleToggle(index)}
+              >
+                <img
+                  className="accordionMenuIcon"
+                  src="https://cdn-icons-png.flaticon.com/128/739/739249.png"
+                  alt=""
+                />
+                &nbsp; {title}
+              </button>
+
+              <div
+                className="panel"
+                style={{
+                  maxHeight: activeIndex === index ? "200px" : "0",
+                  overflow: "hidden",
+                  transition: "max-height 0.3s ease",
+                }}
+              >
+                <ul>
+                  <li>
+                    <a href="#" className="accordionItem">
+                      T-Shirt 1
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#" className="accordionItem">
+                      T-Shirt 2
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
   }
 }
 
-export default MegaMenu
+export default MegaMenu;
--- react/src/pages/HomePage.jsx
+++ react/src/pages/HomePage.jsx
@@ -3,15 +3,19 @@ import FeaturedProduct from '../components/home/FeaturedProduct'
 import { Categories } from './../components/home/Categories';
 import { Collections } from './../components/home/Collections';
 import { NewArrival } from './../components/home/NewArrival';
+import HomeTop from '../components/home/HomeTop';
+import { NavMenuDesktop } from './../components/common/NavMenuDesktop';
 
 export class HomePage extends Component {
   render() {
     return (
       <Fragment>
+         <NavMenuDesktop />
+         <HomeTop />
          <FeaturedProduct />
          <NewArrival />
-         <Collections />
-         <Categories />
+          <Categories />
+          <Collections />
       </Fragment>
     )
   }
