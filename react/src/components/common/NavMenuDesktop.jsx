4848277a5d11548ab958dbc79c4ba8d3060c1eb7
Developerzakir <zakir1234@!za>
2025-11-07 22:19:52 +0600

    Mobile menu and menu responsive design in react

--- react/src/components/common/NavMenuDesktop.jsx
+++ react/src/components/common/NavMenuDesktop.jsx
@@ -3,8 +3,39 @@ import React, { Component, Fragment } from 'react'
 import {Navbar,Container, Row, Col,Button} from 'react-bootstrap';
 import Logo from '../../assets/images/shop.png';
 import {Link} from "react-router-dom";
+import MegaMenuAll from '../home/MegaMenuAll';
 
 export class NavMenuDesktop extends Component {
+   constructor(){
+          super();
+          this.state={
+               SideNavState: "sideNavClose",
+               ContentOverState: "ContentOverlayClose"
+          }
+     }
+
+
+     MenuBarClickHandler=()=>{
+          this.SideNavOpenClose();
+     }
+
+     ContentOverlayClickHandler=()=>{
+          this.SideNavOpenClose();
+     }
+
+
+     SideNavOpenClose=()=>{
+          let SideNavState = this.state.SideNavState;
+          let ContentOverState = this.state.ContentOverState;
+          if(SideNavState==="sideNavOpen"){
+               this.setState({SideNavState:"sideNavClose",ContentOverState:"ContentOverlayClose"})
+
+          }
+          else{
+               this.setState({SideNavState:"sideNavOpen",ContentOverState:"ContentOverlayOpen"})
+          }
+     }
+
   render() {
     return (
       <Fragment>
@@ -14,6 +45,7 @@ export class NavMenuDesktop extends Component {
             <Container fluid={"true"}  className="fixed-top shadow-sm p-2 mb-0 bg-white">
               <Row>
               <Col lg={4} md={4} sm={12} xs={12}>
+              <Button onClick={this.MenuBarClickHandler} className="btn"><i className="fa fa-bars"></i>  </Button>
                 <Link to="/"> <img className="nav-logo" src={Logo} /> </Link>
               </Col>
 
@@ -41,6 +73,14 @@ export class NavMenuDesktop extends Component {
             </Container>
 
           </Navbar>
+        </div>
+
+        <div className={this.state.SideNavState}>
+          <MegaMenuAll />
+        </div>
+
+        <div onClick={this.ContentOverlayClickHandler} className={this.state.ContentOverState}>
+
         </div>
 
       </Fragment>
--- react/src/pages/HomePage.jsx
+++ react/src/pages/HomePage.jsx
@@ -5,17 +5,27 @@ import { Collections } from './../components/home/Collections';
 import { NewArrival } from './../components/home/NewArrival';
 import HomeTop from '../components/home/HomeTop';
 import { NavMenuDesktop } from './../components/common/NavMenuDesktop';
+import NavMenuMobile from '../components/common/NavMenuMobile';
+import HomeTopMobile from '../components/home/HomeTopMobile';
 
 export class HomePage extends Component {
   render() {
     return (
       <Fragment>
-         <NavMenuDesktop />
-         <HomeTop />
+         <div className="Desktop">
+          <NavMenuDesktop />
+          <HomeTop />
+         </div>
+
+          <div className="Mobile">
+              <NavMenuMobile />  
+              <HomeTopMobile />
+          </div>  
+
          <FeaturedProduct />
          <NewArrival />
-          <Categories />
-          <Collections />
+         <Categories />
+         <Collections />
       </Fragment>
     )
   }
