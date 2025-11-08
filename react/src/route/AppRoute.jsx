19e61a5b3f49146749ca65f6f995fc4cd1730310
Developerzakir <zakir1234@!za>
2025-11-08 19:17:47 +0600

    user login page, contact page,privacy policy,refund,how to purchase page design

--- react/src/route/AppRoute.jsx
+++ react/src/route/AppRoute.jsx
@@ -1,13 +1,23 @@
 import React, { Component, Fragment } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import HomePage from '../pages/HomePage.jsx'
+import UserLoginPage from '../pages/UserLoginPage';
+import ContactPage from '../pages/ContactPage';
+import PrivacyPage from '../pages/PrivacyPage';
+import PurchasePage from '../pages/PurchasePage';
+import RefundPage from '../pages/RefundPage';
 
 export class AppRoute extends Component {
   render() {
     return (
      <Fragment>
         <Routes>
-           <Route path="/" element={<HomePage />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<UserLoginPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/purchase" element={<PurchasePage />} />
+          <Route path="/privacy" element={<PrivacyPage />} />
+          <Route path="/refund" element={<RefundPage />} />
         </Routes>
      </Fragment>
     )
