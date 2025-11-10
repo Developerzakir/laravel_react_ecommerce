c3b57aee9a373a6852f7dac7061c8884a28d64bc
Developerzakir <zakir1234@!za>
2025-11-10 09:02:59 +0600

    Contact form Api created in backend

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\ContactController;
 use App\Http\Controllers\Admin\VisitorController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -8,3 +9,4 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/getvisitors',[VisitorController::class,'getVisitorDetails']);
+Route::post('/insert-contact',[ContactController::class,'insertContactDetails']);
